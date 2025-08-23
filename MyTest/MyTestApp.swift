207251d4516dbe20dee33a53cb1e64e60959d85e
Derwood Freen <freenderwood@gmail.com>
2025-08-23 21:40:08 +0100

    Initial Commit

---
+++ MyTest/MyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestApp.swift
+//  MyTest
+//
+//  Created by me developer on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
