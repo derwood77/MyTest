393276eeca2c8c8eb1f76e5536b9d4014a32a722
Derwood Freen <freenderwood@gmail.com>
2025-08-27 16:22:42 +0100

    Added sound capability

--- MyTest/ContentView.swift
+++ MyTest/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
+        
         VStack {
             
             Image(systemName: "paperplane.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, peter")
+            
+            Button("click me")
+            {
+                print("you clicked")
+                let soundName = "sound4"
+                guard let soundFile = NSDataAsset(name: soundName)else {
+                    return print("file not found")
+                }
+                
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                    
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
+        .font(.largeTitle)
         .padding()
     }
 }
